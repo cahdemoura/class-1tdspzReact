64b4e86b2a138e766d7d5a2f9e3ba405281b3bad
CahMoura <Caique.demoura0@gmail.com>
2023-10-27 17:25:41 -0300

    vite react class

---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+import Menu from '../Componentes/menu'
+
+import { TitlePage } from '../Estilos/Estilo'
+
+export default ()=>{
+    return(
+        <>
+
+        <Menu />
+
+        <hr/>
+        <TitlePage>Home</TitlePage>
+        
+<p>Lorem ipsum, dolor sit </p>
+<p>Lorem, ipsum dolor.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, commodi.</p>
+
+        </>
+    )
+}
---
+++ src/pages/sobre.jsx
@@ -0,0 +1,11 @@
+import Menu from '../Componentes/menu'
+
+
+export default ()=>{
+    return(
+        <>
+        <Menu />
+        <h1>pagina sobre</h1>
+        </>
+    )
+}
